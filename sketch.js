223fa1c12db2433634498ceeb60aaae405819108
pix3lbabe <mystudentemail.unt@gmail.com>
2025-04-30 12:32:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ let angle1 = 0.0;
 let segLength = 50;
 
 // Base scale of fox
-let baseScale = 0.05; // Starting small
-let startingScale = 0.05; // Store original size for reset
+let baseScale = 0.02; // Starting small
+let startingScale = 0.02; // Store original size for reset
 
 function preload() {
   fox = loadImage("fox2.png"); //imagine file goes here
@@ -49,7 +49,7 @@ function draw() {
 
 function mousePressed() {
   forestVideo.volume(1); // turn on sound
-  baseScale += 0.05; // Grow the fox when clicked
+  baseScale += 0.02; // Grow the fox when clicked
 }
 
 function keyPressed() {
